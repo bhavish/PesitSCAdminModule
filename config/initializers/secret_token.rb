25931067a1b6d1680c93dcec8b4bbbcd43265244
bhavish <bhavish.agarwal@gmail.com>
2012-08-05 12:23:10 +0530

    start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pessevtu::Application.config.secret_token = '5bed7cd2e6e2bf0a43e417d9d31791511cc64fe6ecf7f79242931dae766d90230857e6b7ea720f7a7c519b19f9cc75a75502e72ba19ae86ab62f0ea311c07d7a'
